80f41ff32854005d7f5247c0092e93bcac3f1a4f
KGoki <kamil.golaszewski.ncf@gmail.com>
2021-05-11 15:17:03 -0400

    Bug attempt fix #4 re:socket config

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 // const PORT = process.env.PORT || 4000
-const URL = process.env.PORT ? "https://spycard-socketserver.herokuapp.com/" : "http://localhost:4000"
-// const URL = "https://spycard-socketserver.herokuapp.com/"
+// const URL = process.env.PORT ? "https://spycard-socketserver.herokuapp.com/" : "http://localhost:4000"
+const URL = "https://spycard-socketserver.herokuapp.com/"
 
 export const socket = io(URL);
 
