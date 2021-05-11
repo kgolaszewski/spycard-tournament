2bd2bfa323e685e920c42f66b3abd652e56d24b9
KGoki <kamil.golaszewski.ncf@gmail.com>
2021-05-11 13:39:13 -0400

    Update config for dual app Heroku deploy

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,8 @@
 import io from "socket.io-client";
 const PORT = process.env.PORT || 4000
-// const PORT = 4000
+const URL = process.env.NODE_ENV !== "production" ? 
+    `http://localhost:${PORT}` 
+        : 
+    `https://spycard-socketserver.herokuapp.com:${PORT}`
 
-export const socket = io(`http://localhost:${PORT}`);
+export const socket = io(URL);
